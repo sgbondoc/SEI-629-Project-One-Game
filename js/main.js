2c106df2875e0fea2c542b4c56c114f893ccec58
sbgarcia <sharonbondoc.g@gmail.com>
2020-07-18 22:25:28 -0700

    project one - dom elements and shuffle function updates

--- js/main.js
+++ js/main.js
@@ -12,11 +12,175 @@ console.log("Hello")
     // "START" button
     // 12 cards
     
-// GAME PLAY
-
-    // To start game:
     // Cards should be placed in 12 random positions face down
     // OPTIONAL: Flash the front of cards at same time and flip back over to back of cards
+    
+// Create object for cards and placement
+let gameSetup = {
+
+    cardValues: ['burger-king', 'five-guys', 'in-n-out', 'kfc', 'mcdonalds', 'nathans', 'burger-king', 'five-guys', 'in-n-out', 'kfc', 'mcdonalds', 'nathans'],
+    
+    divContainers: [],
+
+    frontCards: [],
+
+    backCards: [],
+
+    divCardElements: [],
+    
+    frontImageElements: [],
+
+    backImageElements: [],
+
+    memoryCardElements: [],
+    
+    cardContainers: function () {
+        for (let i = 0; i < this.cardValues.length; i ++) {
+            let container = {
+                class: 'card-container',
+                id: 'div' + i,
+                title: this.cardValues[i]
+            }
+            this.divContainers.push(container)
+        }
+    },
+    
+    frontImages: function () {
+        for (let i = 0; i < this.cardValues.length; i++) {
+            let image = {
+                class:'front-card',
+                src: 'images/card' + i + '.jpg',
+                alt: this.cardValues[i],
+                value: this.cardValues[i]
+            }
+            this.frontCards.push(image)
+        }
+    },
+    
+    backImages: function () {
+        for (let i = 0; i < this.cardValues.length; i++) {
+            let image = {
+                class:'back-card',
+                src: 'images/back.jpg',
+                alt: 'memory-card',
+                value: this.cardValues[i]
+            }
+            this.backCards.push(image)
+        }    
+    },
+    
+    createDivs: function () {
+        for (let i = 0; i < this.cardValues.length; i++) {
+            let createCardDivs = document.createElement('div')
+            createCardDivs.setAttribute ('class', this.divContainers[i].class)
+            createCardDivs.setAttribute ('id', this.divContainers[i].id)
+            createCardDivs.setAttribute ('title', this.divContainers[i].title)
+            this.divCardElements.push(createCardDivs)                
+        }
+    },
+
+
+    createFront: function () {
+        for (let i = 0; i < this.cardValues.length; i++) {
+            let createImage = document.createElement('img')
+            createImage.setAttribute ('class', this.frontCards[i].class) 
+            createImage.setAttribute ('src', this.frontCards[i].src) 
+            createImage.setAttribute ('alt', this.frontCards[i].alt) 
+            createImage.setAttribute ('value', this.frontCards[i].value) 
+            this.frontImageElements.push(createImage)
+        }
+    },    
+    
+    createBack: function () {
+        for (let i = 0; i < this.cardValues.length; i++) {
+            let createImage = document.createElement('img')
+            createImage.setAttribute ('class', this.backCards[i].class) 
+            createImage.setAttribute ('src', this.backCards[i].src) 
+            createImage.setAttribute ('alt', this.backCards[i].alt) 
+            createImage.setAttribute ('value', this.backCards[i].value) 
+            this.backImageElements.push(createImage)
+        }
+    },    
+    
+    appendDiv: function () {
+        for (let i = 0; i < this.cardValues.length; i++) {
+            const main = document.querySelector('.game-board')
+            main.appendChild(this.divCardElements[i])
+        }    
+    },
+
+    appendFront: function () {
+        for (let i = 0; i < this.cardValues.length; i++) {
+            let card = document.querySelector(`[id="${this.divContainers[i].id}"]`)
+            card.appendChild(this.frontImageElements[i])
+        }
+    },
+
+    appendBack: function () {
+        for (let i = 0; i < this.cardValues.length; i++) {
+            let card = document.querySelector(`[id="${this.divContainers[i].id}"]`)
+            card.appendChild(this.backImageElements[i])
+        }
+    },
+
+    allCardElements: function () {
+        for (let i = 0; i < this.cardValues.length; i++) {
+            let memoryCardElement = {
+                div: this.divCardElements[i],
+                frontCard: this.frontImageElements[i],
+                backCard: this.backImageElements[i]
+            }
+            this.memoryCardElements.push(memoryCardElement)
+        }
+    },
+
+    // fisher-yates shuffle (https://bost.ocks.org/mike/shuffle/)
+    shuffle: function (array) {
+        let m = array.length
+        let t = ''
+        let i = ''
+        
+        while (m) {
+            i = Math.floor(Math.random() * m--)
+            t = array[m]
+            array[m] = array[i]
+            array[i] = t
+        }
+        
+        return array
+    },
+
+    appendShuffleElements: function () {
+        for (let i = 0; i < this.cardValues.length; i++) {
+            const main = document.querySelector('.game-board')
+            main.appendChild(this.memoryCardElements[i].div)
+        }    
+    }
+}    
+
+// Call functions to create, place, and append memory game elements
+
+// array of each card container attributes and images
+gameSetup.cardContainers()
+gameSetup.frontImages()
+gameSetup.backImages()
+
+// array of each element
+gameSetup.createDivs()
+gameSetup.createFront()
+gameSetup.createBack()
+
+// append each element into postion on game board
+const main = document.querySelector('.game-board')
+gameSetup.appendDiv()
+gameSetup.appendFront()
+gameSetup.appendBack()
+
+// all game elements set up in object
+gameSetup.allCardElements()
+    
+        
+// GAME PLAY
 
 let time = 30
 let matchCount = 0
@@ -26,31 +190,15 @@ let clickedCardValue = ''
 let clickedValues = []
 
 const clickCards = document.querySelectorAll('.card-container')
-       
-// Create array for front of cards
-// Shuffle cards => NEED TO DO
 
-let cards = []
+// To start game:
 
-const getCards = () => {
-    for (let i = 0; i <= 11; i++) {
-        let image = {
-            imageSource: 'images/card' + i + '.jpg',
-            imageValue: i
-        }
-        cards.push(image)
-    }
-}
-
-getCards()
-
-// Shuffle array of cards to random positions - function
-
-// User clicks on start button:
+    // User clicks on start button:
     // Add event listener to "start" button
     // Game board becomes clickable
     // Timer starts
     // Match counter set at 0
+    // Shuffle array of cards to random positions
     // Reset game board => NEED TO DO
 
 const setTimer = () => {
@@ -66,18 +214,25 @@ const setTimer = () => {
             document.querySelector('#timer').innerHTML = ('TIMER: ' + time + ' SECONDS')
             document.querySelector('#timer').style.color = 'white'
             setTimeout(function() { alert('Time is up! MATCH COUNT: ' + matchCount + '/6') }, 1000)
-            clickCards.removeEventListener('click', flipCard)
+            // clickCards.removeEventListener('click', flipCard)
         }
     }, 1000)
 
     // event listener for clicks on cards
     clickCards.forEach(clickCard => clickCard.addEventListener('click', flipCard))
 }
- 
+    
 // event listeners for click on start button
-document.querySelector('button').addEventListener('click', setTimer)
-// document.querySelector('button').addEventListener('click' resetGame)
+document.querySelector('button').addEventListener('click', function () {
+    setTimer()
 
+    gameSetup.shuffle(gameSetup.memoryCardElements)
+    console.log(gameSetup.memoryCardElements)
+    
+    gameSetup.appendShuffleElements()
+})
+
+// document.querySelector('button').addEventListener('click' resetGame)
 
 // User clicks on 2 cards at a time:
 
@@ -131,11 +286,5 @@ const checkMatch = (clickedValues) => {
         clickedValues.length = 0
     }
 }
-
-// console.log(clickCards)   // all elements with class card-container
-// console.log(clickedCard)  // last clicked card back-card img
-// console.log(clickedCardValue)   // last clicked card value only
-// console.log(clickedValues)  // array of both values only from last 2 clicked cards
-
 
 // OPTIONAL: additional rounds
